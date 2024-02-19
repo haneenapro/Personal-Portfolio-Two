739f0a962f135c59cb7b0bf165d893de37268750
haneenapro <zhaneena@gmail.com>
2024-02-19 20:01:12 +0545

    Init

---
+++ react-portofolio-with-tailwind-main/pages/_app.js
@@ -0,0 +1,8 @@
+import "../styles/globals.css";
+import "../components/Services.module.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
